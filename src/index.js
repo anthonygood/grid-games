9f41f193f3057dc439aa5aa8fa8c1a066fa585de
Anthony Good <good.anthony@gmail.com>
2020-01-16 19:36:08 +0000

    implement tick

--- src/index.js
+++ src/index.js
@@ -8,4 +8,31 @@ class GameOfLife {
   }
 }
 
-module.exports = GameOfLife
+const countNeighbours = ([x, y], bitmap) => {
+  let count = 0
+  for (let i = x - 1; i <= x + 1; i++) {
+    for (let j = y - 1; j <= y + 1; j++) {
+      if (i === x && j === y) continue
+      const neighbour = bitmap[i] && bitmap[i][j]
+      if (neighbour) count += neighbour
+    }
+  }
+  return count
+}
+
+const willLive = (isAlive, neighbourCount) =>
+  neighbourCount === 3 || isAlive && neighbourCount === 2
+
+const liveOrDie = (isAlive, neighbourCount) => Number(willLive(isAlive, neighbourCount))
+
+const tick = bitmap => bitmap.map(
+  (row, i) => row.map(
+    (aliveOrDead, j) => liveOrDie(aliveOrDead, countNeighbours([i,j], bitmap))
+  )
+)
+
+module.exports = {
+  countNeighbours,
+  liveOrDie,
+  tick
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,81 @@
 
 const expect = require('chai').expect
-const GameOfLife = require('./')
+const { countNeighbours, liveOrDie, tick } = require('./')
 
 describe('GameOfLife', () => {
-  it('can be instantiated', () => {
-    const game = new GameOfLife([])
-    expect(game instanceof GameOfLife).to.be.true
+  const bitmap = [
+    [0,1],
+    [1,1]
+  ]
+
+  const bigBitmap = [
+    [0,1,0,0],
+    [0,1,1,1],
+    [1,0,1,0],
+    [0,1,0,1],
+  ]
+
+  describe('countNeighbours', () => {
+    const countNeighourMap = arr => arr.map(
+      (row, i) => row.map(
+        (_, j) => countNeighbours([i,j], arr)
+      )
+    )
+
+    it('returns the number of living neighbours', () => {
+      expect(
+        countNeighourMap(bitmap)
+      ).to.deep.equal([
+        [3,2],
+        [2,2]
+      ])
+
+      expect(
+        countNeighourMap(bigBitmap)
+      ).to.deep.equal([
+        [2,2,4,2],
+        [3,4,4,2],
+        [2,5,5,4],
+        [2,2,3,1]
+      ])
+    })
+  })
+
+  describe('liveOrDie', () => {
+    it('returns correct state for living cells', () => {
+      expect(
+        [0,1,2,3,4,5,6].map(count => liveOrDie(1, count))
+      ).to.deep.equal([
+        0,0,1,1,0,0,0
+      ])
+    })
+
+    it('returns correct state for dead cells', () => {
+      expect(
+        [0,1,2,3,4,5,6].map(count => liveOrDie(0, count))
+      ).to.deep.equal([
+        0,0,0,1,0,0,0
+      ])
+    })
+  })
+
+  describe('tick', () => {
+    it('returns correct state for bitmap', () => {
+      expect(
+        tick(bitmap)
+      ).to.deep.equal([
+        [1,1],
+        [1,1]
+      ])
+
+      expect(
+        tick(bigBitmap)
+      ).to.deep.equal([
+        [0,1,0,0],
+        [1,0,0,1],
+        [1,0,0,0],
+        [0,1,1,0],
+      ])
+    })
   })
 })
