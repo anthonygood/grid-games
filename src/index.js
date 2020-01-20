4150c302fea405275fa0cdd5cfe2bd59991c9108
Anthony Good <good.anthony@gmail.com>
2020-01-20 09:31:55 +0000

    add new index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const Grid = require('./Grid')
+const GameOfLife = require('./GameOfLife')
+const { Minesweeper } = require('./Minesweeper')
+
+module.exports = {
+  Grid,
+  GameOfLife,
+  Minesweeper
+}
