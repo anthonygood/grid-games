d160f1fdfe6bb46aa31406f82e978b11fa10eacf
anthonygood <anthony@hedgehog-invest.com>
2022-09-22 10:08:29 +0100

    small refactor

--- src/Tetris.integration.test.js
+++ src/Tetris.integration.test.js
@@ -8,7 +8,9 @@ describe('Tetris', () => {
 
     const cue = tetromino => tetris.randomTetromino = () => tetromino
     const eventRegister = {}
-    const record = key => event => eventRegister[key] = (eventRegister[key] || []).push(event)
+    const record = key => event => {
+      eventRegister[key] = (eventRegister[key] || []).push(event)
+    }
     tetris.on(Tetris.Events.GAME_OVER, record('gameover'))
     tetris.on(Tetris.Events.LINE_CLEAR, record('lineclear'))
     tetris.on(Tetris.Events.TETROMINO_LANDING, record('tetrominolanding'))
@@ -48,6 +50,32 @@ describe('Tetris', () => {
     expect(tetris.board).to.deep.equal([
       [0,0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [1,1,0,0,0,0,0,0,0,0],
+      [1,1,0,0,0,0,0,0,0,0],
+    ])
+
+    cue(Tetromino.L.reverse())
+    tetris.tick()
+
+    expect(tetris.compositeBoard()).to.deep.equal([
+      [0,0,0,1,1,1,0,0,0,0],
+      [0,0,0,1,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0,0],
