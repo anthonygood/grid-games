9430f5afa578147dac45f5a547190f637c9d7221
Anthony Good <good.anthony@gmail.com>
2022-09-24 07:27:24 +0100

    expose Tetris module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const Grid = require('./Grid')
 const GameOfLife = require('./GameOfLife')
 const { Minesweeper } = require('./Minesweeper')
+const { Tetris } = require('./Tetris')
 
 module.exports = {
   Grid,
   GameOfLife,
-  Minesweeper
+  Minesweeper,
+  Tetris,
 }
